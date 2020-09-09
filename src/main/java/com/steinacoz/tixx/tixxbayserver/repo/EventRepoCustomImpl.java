00bb634c706b6befda428abf25eba0dd4afeaeee
nkenna <nkennannadi@gmail.com>
2020-09-09 11:44:46 +0100

    added create admin api

--- src/main/java/com/steinacoz/tixx/tixxbayserver/repo/EventRepoCustomImpl.java
+++ src/main/java/com/steinacoz/tixx/tixxbayserver/repo/EventRepoCustomImpl.java
@@ -233,7 +233,7 @@ public class EventRepoCustomImpl implements EventRepoCustom {
     public List<EventDao> aggregateAllEventsByName(String title) {
         LocalDateTime now = LocalDateTime.now();
         List<AggregationOperation> list = new ArrayList<AggregationOperation>();//eventCode
-        MatchOperation match = Aggregation.match(Criteria.where("title").regex(title, "i").orOperator(Criteria.where("lga").regex(title, "i"), Criteria.where("state").regex(title, "i"), Criteria.where("country").regex(title, "i")));
+        MatchOperation match = Aggregation.match(Criteria.where("title").regex(title, "i").orOperator(Criteria.where("lga").regex(title, "i"), Criteria.where("state").regex(title, "i"), Criteria.where("country").regex(title, "i"), Criteria.where("categoryName").regex(title, "i")));
         list.add(Aggregation.lookup("childTicket", "eventCode", "eventCode", "childtickets"));
         list.add(Aggregation.lookup("user", "creatorUsername", "username", "createdBy"));
         list.add(Aggregation.lookup("eventTeam", "eventCode", "eventCode", "teams"));
@@ -250,6 +250,7 @@ public class EventRepoCustomImpl implements EventRepoCustom {
     
     
 }
+
 
 
 
