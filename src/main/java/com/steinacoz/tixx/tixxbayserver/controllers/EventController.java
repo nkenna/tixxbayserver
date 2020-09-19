1cd5e0510c7323bc90d33010424b856ad4e4f1d7
nkenna <nkennannadi@gmail.com>
2020-09-19 09:37:49 +0100

    new deploy

--- src/main/java/com/steinacoz/tixx/tixxbayserver/controllers/EventController.java
+++ src/main/java/com/steinacoz/tixx/tixxbayserver/controllers/EventController.java
@@ -216,7 +216,7 @@ public class EventController {
                 Email to = new Email(user.getEmail());
                 Content content = new Content("text/plain", "Your new Event have been successfully created. The event details is available on your dashboard.");
                 Mail mail = new Mail(from, subject, to, content);
-                mail.addCustomArg("content", content.getValue()); //personalization.get(0).addSubstitution("content", content.getValue());
+                mail.addCustomArg("content", "Your new Event have been successfully created. The event details is available on your dashboard."); //personalization.get(0).addSubstitution("content", content.getValue());
                 mail.setTemplateId("d-3645665f028347c286f2ff2f40438c7a");
                 System.out.println(mail.from.getEmail());
                         SendGrid sg = new SendGrid(System.getenv("SENDGRID_API")); 
@@ -787,6 +787,7 @@ public class EventController {
     
     
 }
+
 
 
 
