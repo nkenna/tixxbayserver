27a1c3454c385b8ad62f323f50169f15574a28e3
nkenna <nkennannadi@gmail.com>
2020-09-27 09:35:04 +0100

    added coupon

---
+++ src/main/java/com/steinacoz/tixx/tixxbayserver/errorhandles/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.steinacoz.tixx.tixxbayserver.errorhandles;
+
+import com.steinacoz.tixx.tixxbayserver.errormodels.AccessDeniedException;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ *
+ * @author nkenn
+ */
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleAccessDeniedException(
+      Exception ex, WebRequest request) {
+        return new ResponseEntity<Object>(
+          "Access denied message here", new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+    
+    
+    
+    
+}
+
+
+
---
+++ src/main/java/com/steinacoz/tixx/tixxbayserver/errormodels/AccessDeniedException.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.steinacoz.tixx.tixxbayserver.errormodels;
+
+/**
+ *
+ * @author nkenn
+ */
+public class AccessDeniedException {
+    private String status;
+    private String message;
+    private int errorCode;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+    
+    
+}
+
+
